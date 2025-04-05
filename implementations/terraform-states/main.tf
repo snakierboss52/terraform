56a5c6c309d082e3311c905b21359903c4be7763
Jorge Lozano <jorge@MacBook-Pro-de-Jorge.local>
2025-04-05 18:19:42 -0500

    feat: add changes

---
+++ implementations/terraform-states/main.tf
@@ -0,0 +1,67 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-terragrunt-states"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terragrunt-state"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "aws_s3_bucket_policy" "terraform_state" {
+  bucket = "${aws_s3_bucket.terraform_state.id}"
+  policy =<<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "RequireEncryption",
+   "Statement": [
+    {
+      "Sid": "RequireEncryptedTransport",
+      "Effect": "Deny",
+      "Action": ["s3:*"],
+      "Resource": ["arn:aws:s3:::${aws_s3_bucket.terraform_state.bucket}/*"],
+      "Condition": {
+        "Bool": {
+          "aws:SecureTransport": "false"
+        }
+      },
+      "Principal": "*"
+    },
+    {
+      "Sid": "RequireEncryptedStorage",
+      "Effect": "Deny",
+      "Action": ["s3:PutObject"],
+      "Resource": ["arn:aws:s3:::${aws_s3_bucket.terraform_state.bucket}/*"],
+      "Condition": {
+        "StringNotEquals": {
+          "s3:x-amz-server-side-encryption": "AES256"
+        }
+      },
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+}
